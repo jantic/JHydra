fbf420f7fb12ba48605c6eaf136573a813e823cb
Jason Antic <jsa169@gmail.com>
2012-12-21 22:45:25 -0500

    Added more ValueMap junit tests.

--- test/jhydra/core/valuemap/ValueMapTests.java
+++ test/jhydra/core/valuemap/ValueMapTests.java
@@ -4,14 +4,11 @@
  */
 package jhydra.core.valuemap;
 
-import java.util.List;
 import jhydra.core.FatalException;
 import jhydra.core.config.IConfig;
 import jhydra.core.lexicon.ILexicon;
 import jhydra.core.lexicon.Lexicon;
 import jhydra.core.lexicon.NameNotInLexiconException;
-import jhydra.core.properties.DuplicatedKeyException;
-import jhydra.core.properties.INameValue;
 import jhydra.core.properties.NameNotValidException;
 import org.junit.Assert;
 import org.junit.Test;
@@ -26,7 +23,7 @@ public class ValueMapTests {
     public ValueMapTests() {
     }
     
-    /***Tests using correctly configured lexicon, no modifications of state****************************************/ 
+    /***Tests using correctly configured lexicon, for getValue****************************************/ 
     @Test
     public void getValue_FirstName_Mr() throws FatalException{
         final ILexicon lexicon = getMockedBasicLexicon();
@@ -113,7 +110,138 @@ public class ValueMapTests {
         final String actual = valueMap.getValue("LastName");
         Assert.assertEquals(expected, actual);
     }
-      
+   
+   
+    /***Tests using correctly configured lexicon, for setValue****************************************/ 
+    @Test
+    public void setValue_FirstName_ModifiedOnce_getValue_Smacky() throws FatalException{
+        final ILexicon lexicon = getMockedBasicLexicon();
+        final IValueMap valueMap = new ValueMap(lexicon);
+        valueMap.setValue("FirstName", "Smacky");
+        final String expected = "Smacky";
+        final String actual = valueMap.getValue("FirstName");
+        Assert.assertEquals(expected, actual);
+    }
+    
+    @Test
+    public void setValue_FirstName_ModifiedTwice_getValue_The_Frog() throws FatalException{
+        final ILexicon lexicon = getMockedBasicLexicon();
+        final IValueMap valueMap = new ValueMap(lexicon);
+        valueMap.setValue("FirstName", "Smacky");
+        valueMap.setValue("FirstName", "The Frog");
+        final String expected = "The Frog";
+        final String actual = valueMap.getValue("FirstName");
+        Assert.assertEquals(expected, actual);
+    }
+    
+    @Test
+    public void setValue_SpecialChars_getValue_CorrectValue() throws FatalException{
+        final ILexicon lexicon = getMockedBasicLexicon();
+        final IValueMap valueMap = new ValueMap(lexicon);
+        final String newValue = "!#$%^&*()-_}{][\\|/><.,;\"-+.:?=";
+        valueMap.setValue("FirstName", newValue);
+        final String expected = newValue;
+        final String actual = valueMap.getValue("FirstName");
+        Assert.assertEquals(expected, actual);
+    }
+    
+    @Test
+    public void setValue_NumericalChars_getValue_1234567890() throws FatalException{
+        final ILexicon lexicon = getMockedBasicLexicon();
+        final IValueMap valueMap = new ValueMap(lexicon);
+        final String newValue = "1234567890";
+        valueMap.setValue("FirstName", newValue);
+        final String expected = newValue;
+        final String actual = valueMap.getValue("FirstName");
+        Assert.assertEquals(expected, actual);
+    }
+    
+    @Test
+    public void setValue_null_getValue_empty() throws FatalException{
+        final ILexicon lexicon = getMockedBasicLexicon();
+        final IValueMap valueMap = new ValueMap(lexicon);
+        valueMap.setValue("FirstName", null);
+        final String expected = "";
+        final String actual = valueMap.getValue("FirstName");
+        Assert.assertEquals(expected, actual);
+    }
+    
+    @Test
+    public void setValue_empty_getValue_empty() throws FatalException{
+        final ILexicon lexicon = getMockedBasicLexicon();
+        final IValueMap valueMap = new ValueMap(lexicon);
+        valueMap.setValue("FirstName", "");
+        final String expected = "";
+        final String actual = valueMap.getValue("FirstName");
+        Assert.assertEquals(expected, actual);
+    }
+    
+    @Test(expected = NameNotInLexiconException.class)
+    public void setValue_invalidName_NameNotInLexiconException() throws FatalException{
+        final ILexicon lexicon = getMockedBasicLexicon();
+        final IValueMap valueMap = new ValueMap(lexicon);
+        valueMap.setValue("Derp", "");
+    }
+    
+   /***Tests using correctly configured lexicon, for hasValue****************************************/ 
+    
+   @Test
+    public void hasValue_FirstName_true() throws FatalException{
+        final ILexicon lexicon = getMockedBasicLexicon();
+        final IValueMap valueMap = new ValueMap(lexicon);
+        final Boolean expected = true;
+        final Boolean actual = valueMap.hasValue("FirstName");
+        Assert.assertEquals(expected, actual);
+    }
+   
+    @Test
+    public void hasValue_SecondName_false() throws FatalException{
+        final ILexicon lexicon = getMockedBasicLexicon();
+        final IValueMap valueMap = new ValueMap(lexicon);
+        final Boolean expected = false;
+        final Boolean actual = valueMap.hasValue("SecondName");
+        Assert.assertEquals(expected, actual);
+    }
+    
+    @Test
+    public void hasValue_InvalidName_false() throws FatalException{
+        final ILexicon lexicon = getMockedBasicLexicon();
+        final IValueMap valueMap = new ValueMap(lexicon);
+        final Boolean expected = false;
+        final Boolean actual = valueMap.hasValue("First Name");
+        Assert.assertEquals(expected, actual);
+    }
+    
+    
+    @Test
+    public void hasValue_SpecChars_false() throws FatalException{
+        final ILexicon lexicon = getMockedBasicLexicon();
+        final IValueMap valueMap = new ValueMap(lexicon);
+        final Boolean expected = false;
+        final Boolean actual = valueMap.hasValue("!#$%^&*()-_}{][\\|/><.,;\"-+.:?=");
+        Assert.assertEquals(expected, actual);
+    }
+    
+    @Test
+    public void hasValue_null_false() throws FatalException{
+        final ILexicon lexicon = getMockedBasicLexicon();
+        final IValueMap valueMap = new ValueMap(lexicon);
+        final Boolean expected = false;
+        final Boolean actual = valueMap.hasValue(null);
+        Assert.assertEquals(expected, actual);
+    }
+    
+   @Test
+    public void hasValue_empty_false() throws FatalException{
+        final ILexicon lexicon = getMockedBasicLexicon();
+        final IValueMap valueMap = new ValueMap(lexicon);
+        final Boolean expected = false;
+        final Boolean actual = valueMap.hasValue("");
+        Assert.assertEquals(expected, actual);
+    }
+    
+   /***PRIVATE****************************************/
+   
     private ILexicon getMockedBasicLexicon() throws FatalException{
         return new Lexicon(getMockedBasicConfig());
     }
